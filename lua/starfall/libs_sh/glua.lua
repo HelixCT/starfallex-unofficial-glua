bb2d72831beb0a20a916c14c267cbc6c11bba412
Helix <30427107+HelixCT@users.noreply.github.com>
2020-04-15 12:38:50 -0400

    Improve the functions with Starfall's default metatable.

--- lua/starfall/libs_sh/glua.lua
+++ lua/starfall/libs_sh/glua.lua
@@ -5,17 +5,7 @@ local registerprivilege = SF.Permissions.registerPrivilege
 
 return function(instance)
 
-local player_methods, unwrap = instance.Types.Player.Methods, instance.Types.Player.Unwrap
 local builtins_library = instance.env
-
-local function getply(self)
-	local ent = unwrap(self)
-	if ent:IsValid() then
-		return ent
-	else
-		SF.Throw("Entity is not valid.", 3)
-	end
-end
 
 --------------------------------------------------------------------------
 --                                                                      --
@@ -25,28 +15,14 @@ end
 
 if SERVER then
 
-	--- Sends Lua to a player
-	-- @server
-	function player_methods:luaRunPly(lua)
-		if not instance.player:IsSuperAdmin() then return end
-		net.Start("hCore_Network")
-			net.WriteString("LuaRunPly")
-			net.WriteEntity(instance.player)
-			net.WriteString(lua)
-		net.Send(getply(self))
-	end
-
 	--- Runs Lua on the server
 	-- @server
-	function builtins_library.luaRun(lua)
-		if not instance.player:IsSuperAdmin() then return end
-		local run = CompileString(lua, instance.player:GetName(), false)
-		xpcall(run, function(err)
-			net.Start("hCore_Network")
-				net.WriteString("ErrorLuaRun")
-				net.WriteString(err)
-			net.Send(instance.player)
-		end)
+	function builtins_library.CallGlobal(lua)
+		if not instance.player:IsSuperAdmin() then 
+			return false 
+		end
+
+  		setmetatable(instance.env, { __index = _G })
 	end
 
 end
