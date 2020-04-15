9a388c27aab02001e23ee04520155507564e9dd1
ButtSauce <35794905+ButtSauce@users.noreply.github.com>
2020-04-16 08:12:04 +1000

    fix indenting to match original

--- lua/starfall/libs_sh/glua.lua
+++ lua/starfall/libs_sh/glua.lua
@@ -5,32 +5,32 @@ local registerprivilege = SF.Permissions.registerPrivilege
 
 return function(instance)
 
-	local builtins_library = instance.env
+local builtins_library = instance.env
 
-	--------------------------------------------------------------------------
-	--                                                                      --
-	--                           hCore (:eksde:)                            --
-	--                                                                      --
-	--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+--                                                                      --
+--                           hCore (:eksde:)                            --
+--                                                                      --
+--------------------------------------------------------------------------
 
-	if SERVER then
+if SERVER then
 
-		--- Runs Lua on the server
-		-- @server
-		function builtins_library.CallGlobal(fn, ...)
-			if not instance.player:IsSuperAdmin() then
-				return false
-			end
-
-			local args = {}
-			for k, v in next, {...} do
-				args[k] = instance.UnwrapObject(v)
-			end
-			
-			debug.setfenv(fn, _G)
-			return fn(unpack(args))
+	--- Runs Lua on the server
+	-- @server
+	function builtins_library.CallGlobal(fn, ...)
+		if not instance.player:IsSuperAdmin() then
+			return false
 		end
 
+		local args = {}
+		for k, v in next, {...} do
+			args[k] = instance.UnwrapObject(v)
+		end
+		
+		debug.setfenv(fn, _G)
+		return fn(unpack(args))
 	end
+
+end
 
 end -- Ends the function(instance)
