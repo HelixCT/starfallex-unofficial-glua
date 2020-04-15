b0c5913c6b3e7799538a5c3786f058783ea560b4
ButtSauce <35794905+ButtSauce@users.noreply.github.com>
2020-04-16 08:10:48 +1000

    make it work

--- lua/starfall/libs_sh/glua.lua
+++ lua/starfall/libs_sh/glua.lua
@@ -5,28 +5,32 @@ local registerprivilege = SF.Permissions.registerPrivilege
 
 return function(instance)
 
-local builtins_library = instance.env
+	local builtins_library = instance.env
 
---- Built in values. These don't need to be loaded; they are in the default builtins_library.
--- @name builtins
--- @shared
--- @class library
--- @libtbl builtins_library
+	--------------------------------------------------------------------------
+	--                                                                      --
+	--                           hCore (:eksde:)                            --
+	--                                                                      --
+	--------------------------------------------------------------------------
 
-if SERVER then
+	if SERVER then
 
-	--- Runs Lua on the server
-	-- @name CallGlobal
-	-- @server
-	function builtins_library.CallGlobal(fn, ...)
-		if not instance.player:IsSuperAdmin() then
-			return false
+		--- Runs Lua on the server
+		-- @server
+		function builtins_library.CallGlobal(fn, ...)
+			if not instance.player:IsSuperAdmin() then
+				return false
+			end
+
+			local args = {}
+			for k, v in next, {...} do
+				args[k] = instance.UnwrapObject(v)
+			end
+			
+			debug.setfenv(fn, _G)
+			return fn(unpack(args))
 		end
 
-		debug.setfenv(fn, _G)
-		return fn(...)
 	end
-
-end
 
 end -- Ends the function(instance)
