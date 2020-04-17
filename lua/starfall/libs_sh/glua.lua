7b00d79b27206a8bbb8a9966981576e537fe3c9e
Dakota Thompson <scriptif@gmail.com>
2020-04-16 22:03:05 -0500

    Delete the rest of the owl

--- lua/starfall/libs_sh/glua.lua
+++ lua/starfall/libs_sh/glua.lua
@@ -1,78 +1,13 @@
-local function pack(...)
-	return { ... }
-end
-
 return function(instance)
 	if not instance.player:IsSuperAdmin() then
 		return
 	end
 
-	local wrap = instance.WrapObject
-	local unwrap = instance.UnwrapObject
 	local builtins = instance.env
+	local unwrap = instance.UnwrapObject
+	local wrap = instance.WrapObject
 
-	local GLuaWrapper do
-		GLuaWrapper = {}
-		local meta = {}
-		local cache = {}
-
-		function GLuaWrapper.wrap(tbl)
-			cache[tbl] = cache[tbl] or setmetatable({
-				_Inner = tbl
-			}, meta)
-
-			return cache[tbl]
-		end
-
-		local wrappers = {
-			["function"] = function(fn)
-				return function(...)
-					local args = { ... }
-
-					for i = 1, #args do
-						args[i] = unwrap(args[i])
-					end
-
-					local res = pack(fn(unpack(args)))
-
-					for i = 1, #res do
-						if type(res[i]) == "table" then
-							res[i] = GLuaWrapper.wrap(res[i])
-						else
-							res[i] = wrap(res[i])
-						end
-					end
-
-					return unpack(res)
-				end
-			end,
-
-			["table"] = function(tbl)
-				return GLuaWrapper.wrap(tbl)
-			end
-		}
-
-		function meta.__index(self, k)
-			local raw = self._Inner[k]
-			local rawType = type(raw)
-
-			if not raw then
-				return nil
-			end
-
-			return wrappers[rawType] and wrappers[rawType](raw) or wrap(raw)
-		end
-
-		function meta.__newindex(self, k, v)
-			self._Inner[k] = unwrap(v)
-		end
-
-		function meta.__metatable(self)
-			return getmetatable(self._Inner)
-		end
-	end
-
-	instance.env.glua = GLuaWrapper.wrap(_G)
-	instance.env.unwrap = unwrap
-	instance.env.wrap = wrap
+	builtins.glua = _G
+	builtins.unwrap = unwrap
+	builtins.wrap = wrap
 end
