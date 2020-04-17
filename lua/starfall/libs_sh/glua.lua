115dd9a64ccf6865edd9dd6d96b5555ca54c1f6b
Dakota Thompson <scriptif@gmail.com>
2020-04-16 21:26:52 -0500

    Full rework

--- lua/starfall/libs_sh/glua.lua
+++ lua/starfall/libs_sh/glua.lua
@@ -1,36 +1,67 @@
--- Global to all starfalls
-local checkluatype = SF.CheckLuaType
-local checkpermission = SF.Permissions.check
-local registerprivilege = SF.Permissions.registerPrivilege
-
-return function(instance)
-
-local builtins_library = instance.env
-
---------------------------------------------------------------------------
---                                                                      --
---                           hCore (:eksde:)                            --
---                                                                      --
---------------------------------------------------------------------------
-
-if SERVER then
-
-	--- Runs Lua on the server
-	-- @server
-	function builtins_library.CallGlobal(fn, ...)
-		if not instance.player:IsSuperAdmin() then
-			return false
-		end
-
-		local args = {}
-		for k, v in next, {...} do
-			args[k] = instance.UnwrapObject(v)
-		end
-		
-		debug.setfenv(fn, _G)
-		return fn(unpack(args))
-	end
-
+local function pack(...)
+	return { ... }
 end
 
-end -- Ends the function(instance)
+return function(instance)
+	local wrap = instance.WrapObject
+	local unwrap = instance.UnwrapObject
+	local builtins = instance.env
+
+	if not instance.player:IsSuperAdmin() then
+		return
+	end
+
+	local GLuaWrapper do
+		GLuaWrapper = {}
+		local meta = {}
+		local cache = {}
+
+		function GLuaWrapper.from(tbl)
+			cache[tbl] = cache[tbl] or setmetatable({
+				_Inner = tbl
+			}, meta)
+
+			return cache[tbl]
+		end
+
+		local indexTypedWrappers = {
+			["function"] = function(fn)
+				return function(...)
+					local args = { ... }
+
+					for i = 1, #args do
+						args[i] = unwrap(args[i])
+					end
+
+					local res = pack(fn(unpack(args)))
+
+					for i = 1, #res do
+						res[i] = wrap(res[i])
+					end
+
+					return unpack(res)
+				end
+			end,
+
+			["table"] = function(tbl)
+				return GLuaWrapper.from(tbl)
+			end
+		}
+
+		function meta.__index(self, k, v)
+			local raw = rawget(self, k, v) or rawget(self._Inner, k, v)
+
+			return indexTypedWrappers[raw] and indexTypedWrappers[raw](raw) or wrap(raw)
+		end
+
+		function meta.__newindex(self, k, v)
+			rawset(self._Inner, k, unwrap(v))
+		end
+
+		function meta.__metatable(self, k, v)
+			return false
+		end
+	end
+
+	instance.env.glua = GLuaWrapper.from(_G)
+end
